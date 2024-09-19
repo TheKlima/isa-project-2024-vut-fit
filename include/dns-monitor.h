9cb504366ff6ed6f1464bd5bbc43d3cdf1364de5
Andrii <and.klimenkoo@gmail.com>
2024-09-19 17:59:33 +0200

    added class Dns_monitor and createPcapHandle() inside this class

---
+++ include/dns-monitor.h
@@ -0,0 +1,24 @@
+#ifndef DNS_MONITOR_H
+#define DNS_MONITOR_H
+
+#include "args.h"
+#include <pcap/pcap.h>
+#include <cstring>
+
+class Dns_monitor {
+public:
+    Dns_monitor(int argc, char** argv);
+    
+private:
+    Args m_args;
+    bool m_is_constructor_err{};
+    char m_err_buff[PCAP_ERRBUF_SIZE]{};
+    pcap_t* m_pcap_handle{};
+    const char* const m_dns_filter{};
+
+
+
+    bool createPcapHandle();
+};
+
+#endif // DNS_MONITOR_H
---
+++ src/dns-monitor.cpp
@@ -0,0 +1,61 @@
+#include "dns-monitor.h"
+
+Dns_monitor::Dns_monitor(int argc, char **argv)
+    :
+    m_args{argc, argv},
+    m_is_constructor_err{false},
+    m_err_buff{0, },
+    m_pcap_handle{nullptr},
+    m_dns_filter{"port 53"}
+{
+    
+}
+
+// preparation before processing DNS packets
+// inspired by: https://vichargrave.github.io/programming/develop-a-packet-sniffer-with-libpcap/#process-packets
+bool Dns_monitor::createPcapHandle()
+{
+    bpf_u_int32 net_mask{0};
+    bpf_u_int32 src_ip{0};
+
+    if(m_args.getSniffingFromInterface())
+    {
+        // get network device source IP address and netmask
+        if(pcap_lookupnet(m_args.getPacketsSource(), &src_ip, &net_mask, m_err_buff) == PCAP_ERROR)
+        {
+            return false;
+        }
+
+        // open the device for live capture
+        m_pcap_handle = pcap_open_live(m_args.getPacketsSource(), BUFSIZ, 1, 300, m_err_buff);
+    }
+    else
+    {
+        m_pcap_handle = pcap_open_offline(m_args.getPacketsSource(), m_err_buff);
+    }
+    
+    if(!m_pcap_handle)
+    {
+        return false;
+    }
+
+    struct bpf_program bpf{};
+
+    // convert the packet filter expression into a packet filter binary
+    if(pcap_compile(m_pcap_handle, &bpf, m_dns_filter, 0, net_mask) == PCAP_ERROR)
+    {
+        strcpy(m_err_buff, pcap_geterr(m_pcap_handle));
+        return false;
+    }
+
+    // bind the packet filter to the libpcap handle
+    if(pcap_setfilter(m_pcap_handle, &bpf) == PCAP_ERROR)
+    {
+        strcpy(m_err_buff, pcap_geterr(m_pcap_handle));
+        pcap_freecode(&bpf);
+        return false;
+    }
+
+    pcap_freecode(&bpf);
+    return true;
+}
