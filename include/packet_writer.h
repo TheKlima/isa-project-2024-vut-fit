fd22c7e1769512fc85696a51d69ab13a3b87f8e7
Andrii <and.klimenkoo@gmail.com>
2024-09-23 20:41:00 +0200

    added printTimestamp() to Packet_writer abstract class

--- include/packet_writer.h
+++ include/packet_writer.h
@@ -1,11 +1,16 @@
 #ifndef PACKET_WRITER_H
 #define PACKET_WRITER_H
 
-#include <new>  // For std::nothrow
+#include <new>     // For std::nothrow
+#include <cstdlib> // For u_char
 
 class Packet_writer {
 public:
     static Packet_writer* create(bool is_verbose);
+    virtual void printPacket(struct pcap_pkthdr* packet_header, const u_char* packet_data) override;
+    
+private:
+    bool printTimestamp(struct pcap_pkthdr* packet_header) const;
 };
 
 #endif // PACKET_WRITER_H
--- src/packet_writer.cpp
+++ src/packet_writer.cpp
@@ -1,6 +1,8 @@
 #include "packet_writer.h"
 #include "verbose_packet_writer.h"
 #include "simple_packet_writer.h"
+#include <ctime>                    // For localtime() and strftime()
+#include <iostream>
 
 Packet_writer* Packet_writer::create(bool is_verbose)
 {
@@ -8,6 +10,22 @@ Packet_writer* Packet_writer::create(bool is_verbose)
     {
         return new(std::nothrow) Verbose_packet_writer;
     }
-    
+
     return new(std::nothrow) Simple_packet_writer;
+}
+
+bool Packet_writer::printTimestamp(struct pcap_pkthdr* packet_header) const
+{
+    struct tm* local_time{localtime(&(packet_header->ts.tv_sec))};
+
+    if(local_time == NULL)
+    {
+        return false;
+    }
+
+    char timestamp_buffer[20]{0, };
+    strftime(timestamp_buffer, sizeof(timestamp_buffer), "%Y-%m-%d %H:%M:%S", local_time);
+    
+    std::cout << timestamp_buffer;
+    return true;
 }
