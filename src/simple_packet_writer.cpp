7b0d1794640b0a822226214a0b15693ce58bacfb
Andrii <and.klimenkoo@gmail.com>
2024-09-24 14:41:31 +0200

    updated printPacket() in Simple_packet_writer: now print src and dst ip addresses

--- src/simple_packet_writer.cpp
+++ src/simple_packet_writer.cpp
@@ -3,5 +3,11 @@
 void Simple_packet_writer::printPacket(struct pcap_pkthdr* packet_header, const u_char* packet_data)
 {
     printTimestamp(packet_header);
-    std::cout << ' ';
+    std::cout << ' ' << std::flush;
+    processIpHeader(packet_header, packet_data);
+    printSrcIp();
+    std::cout << " -> " << std::flush;
+    printDstIp();
+    std::cout << " (" << std::flush;
+    std::cout << ')' << std::endl;
 }
