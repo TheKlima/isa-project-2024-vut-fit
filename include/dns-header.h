1c990ec11b99abb22048d13fdbf88380a9b8a1d9
Andrii <and.klimenkoo@gmail.com>
2024-10-05 13:07:59 +0200

    declared new class Dns_header

---
+++ include/dns-header.h
@@ -0,0 +1,25 @@
+#ifndef DNS_HEADER_H
+#define DNS_HEADER_H
+
+#include <cstdint>
+
+class __attribute__((packed)) Dns_header {
+private:
+    uint16_t m_id{};
+    uint16_t m_qr : 1 {};
+    uint16_t m_opcode : 4 {};
+    uint16_t m_aa : 1 {};
+    uint16_t m_tc : 1 {};
+    uint16_t m_rd : 1 {};
+    uint16_t m_ra : 1 {};
+    uint16_t m_z : 1 {};
+    uint16_t m_ad : 1 {};
+    uint16_t m_cd : 1 {};
+    uint16_t m_rcode : 4 {};
+    uint16_t m_qdcount{};
+    uint16_t m_ancount{};
+    uint16_t m_nscount{};
+    uint16_t m_arcount{};
+};
+
+#endif // DNS_HEADER_H
